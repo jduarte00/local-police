cbf1aeae881ebcbbe7f3cc405382cbfd33f16af8
Jose Duarte <dumejo92@gmail.com>
2019-02-06 10:06:06 -0600

    make model for incidents

---
+++ models/Incident.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const incidentSchema = new Schema({
+    location: String,
+    type: String,
+    oficialNumber: String,
+    date: Date,
+    socialGrade: 0
+}, {
+    timestamps:{
+        createdAt: "created_at"
+    }
+});
+
+const Incident = mongoose.model('Incident', incidentSchema);
+module.exports = Incident;
+
