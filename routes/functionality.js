22a3fdddb870675ddbf8af9b612f2fda43fc0903
Jose Duarte <dumejo92@gmail.com>
2019-02-06 10:08:36 -0600

    create route file and set routes to create a new incident and access control panel

---
+++ routes/functionality.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const funcRoutes = express.Router();
+const ensureLogin = require("connect-ensure-login");
+
+const Incident = require("../models/Incident");
+const User = require("../models/User");
+
+funcRoutes.get("/newIncident", ensureLogin.ensureLoggedIn(),(req, res, next) => {
+  res.render("incident");
+});
+
+funcRoutes.post("/newIncident", (req, res, next)=>{
+    const location = req.body.location;
+    const type = req.body.type;
+    const oficialNumber = req.body.oficialNumber;
+    const date = req.body.date;
+    const userID = req.user._id;
+    const newIncident = new Incident({
+      location,
+      type,
+      oficialNumber,
+      date
+    });
+    newIncident.save().then(incident=>{
+      const incidentID = incident._id;
+      User.findById(userID).then(user=>{
+        user.reportedIncidents.push(incidentID);
+        user.save().then(user=>{
+          res.redirect("/");
+        });
+      });
+    });
+});
+
+funcRoutes.get("/controlPanel", ensureLogin.ensureLoggedIn(),(req, res, next) => {
+  const userId = req.user._id;
+  User.findById(userId).populate('reportedIncidents').then(user => {
+    let incidents = user.reportedIncidents;
+    console.log(incidents, "estous osn los incidentes");
+    res.render("control-panel", {incidents});
+  })
+
+});
+
+module.exports = funcRoutes;
